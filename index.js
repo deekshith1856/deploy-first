306e8dc8df1d26f425f57fc92839aef1e8f9c12a
deekshith1856 <deekshith@docupilot.app>
2024-10-19 11:06:47 +0530

    Added test files

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import express from "express";
 
-const app= express()
+const app = express();
 
-app.use('/',(req, res)=>{
-    
-res.send('hello, how are you!')
-})
+app.use("/", (req, res) => {
+  res.status(200).send("Hello, World!");
+});
 
-const port= 5000;
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ test/index.test.js
@@ -0,0 +1,17 @@
+// test/index.test.js
+import request from "supertest";
+import app from "../index"; // Import your Express app
+
+describe("GET /", () => {
+  it("should return Hello, World!", (done) => {
+    request(app)
+      .get("/") // Simulate a GET request to the root endpoint
+      .expect("Content-Type", /text/)
+      .expect(200) // Expect HTTP status 200
+      .expect("Hello, World!") // Expect the body to contain 'Hello, World!'
+      .end((err, res) => {
+        if (err) return done(err); // If there's an error, fail the test
+        done(); // If no error, the test passes
+      });
+  });
+});
